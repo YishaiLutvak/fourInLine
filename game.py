98fccfb850788eef73cd8bca5ee0bfa0354c5267
User <ylutvak@gmail.com>
2020-04-30 18:45:21 +0300

    after alachsonim

--- game.py
+++ game.py
@@ -2,23 +2,23 @@ import copy
 import alphaBetaPruning
 import random
 
-VICTORY=10**20 #The value of a winning board (for max) 
-LOSS = -VICTORY #The value of a losing board (for max)
-TIE=0 #The value of a tie
-SIZE=4 #the length of winning seq.
-COMPUTER=SIZE+1 #Marks the computer's cells on the board
-HUMAN=1 #Marks the human's cells on the board
+VICTORY = 10 ** 20  # The value of a winning board (for max)
+LOSS = -VICTORY  # The value of a losing board (for max)
+TIE = 0  # The value of a tie
+SIZE = 4  # the length of winning seq.
+COMPUTER = SIZE + 1  # Marks the computer's cells on the board
+HUMAN = 1  # Marks the human's cells on the board
 
-rows=6
-columns=7
+rows = 6
+columns = 7
 
 
 class game:
-    board=[]
-    size=rows*columns
+    board = []
+    size = rows * columns
     playTurn = HUMAN
-    
-     #Used by alpha-beta pruning to allow pruning
+
+    # Used by alpha-beta pruning to allow pruning
 
     '''
     The state of the game is represented by a list of 4 items:
@@ -29,161 +29,207 @@ class game:
         3. Number of empty cells
     '''
 
+
 def create(s):
-        #Returns an empty board. The human plays first.
-        #create the board
-        s.board=[]
-        for i in range(rows):
-            s.board = s.board+[columns*[0]]
-        
-        s.playTurn = HUMAN
-        s.size=rows*columns
-        s.val=0.00001
-    
-        #return [board, 0.00001, playTurn, r*c]     # 0 is TIE
+    # Returns an empty board. The human plays first.
+    # create the board
+    s.board = []
+    for i in range(rows):
+        s.board = s.board + [columns * [0]]
+
+    s.playTurn = HUMAN
+    s.size = rows * columns
+    s.val = 0.00001
+
+    # return [board, 0.00001, playTurn, r*c]     # 0 is TIE
+
 
 def cpy(s1):
-        # construct a parent DataFrame instance
-        s2=game()
-        s2.playTurn = s1.playTurn
-        s2.size=s1.size
-        s2.board=copy.deepcopy(s1.board)
-        print("board ", s2.board)
-        return s2
-    
-    
-    
+    # construct a parent DataFrame instance
+    s2 = game()
+    s2.playTurn = s1.playTurn
+    s2.size = s1.size
+    s2.board = copy.deepcopy(s1.board)
+    print("board ", s2.board)
+    return s2
+
+
 def value(s):
-#Returns the heuristic value of s
-    if find_horizontal(s) or find_vertical(s):
+    # Returns the heuristic value of s
+    if find_horizontal(s) or find_vertical(s) or find_hypotenuse1(s) or find_hypotenuse2(s):
         return VICTORY if s.playTurn == HUMAN else LOSS
     if s.size == 0:
         return TIE
     return random.random() * 10
 
 
-def find_horizontal(s:game):
-    turn = {HUMAN:COMPUTER,COMPUTER:HUMAN}
-    return any(map(lambda i: max_len(s.board[i],turn[s.playTurn])>=4,range(rows)))
+def find_horizontal(s: game):
+    turn = {HUMAN: COMPUTER, COMPUTER: HUMAN}
+    return any(map(lambda i: max_len(s.board[i], turn[s.playTurn]) >= 4, range(rows)))
+
 
 def find_vertical(s):
     turn = {HUMAN: COMPUTER, COMPUTER: HUMAN}
-    vertical_func = lambda i: map(lambda x:x[i],s.board)
-    return any(map(lambda i: max_len(vertical_func(i),turn[s.playTurn])>=4,range(columns)))
+    vertical_func = lambda i: map(lambda x: x[i], s.board)
+    return any(map(lambda i: max_len(vertical_func(i), turn[s.playTurn]) >= 4, range(columns)))
 
-def max_len(_list,num):
+
+def hypotenuse1_func(s):
+    column = 0
+    row = rows - 4
+    hypotenuses1 = []
+    hypo = []
+    for i in range(rows + columns - 2 * 3 - 1):
+        step = 0
+        while row + step < rows and column + step < columns:
+            hypo = hypo + [s.board[row + step][column + step]]
+            step += 1
+        hypotenuses1 += [hypo]
+        hypo.clear
+        if row != 0:
+            row -= 1
+        else:
+            column += 1
+    return hypotenuses1
+
+def hypotenuse2_func(s):
+    column = 3
+    row = 0
+    hypotenuses2 = []
+    hypo = []
+    for i in range(rows + columns - 2 * 3 - 1):
+        step = 0
+        while row + step < rows and column - step > 0:
+            hypo = hypo + [s.board[row + step][column - step]]
+            step += 1
+        hypotenuses2 += [hypo]
+        hypo.clear
+        if column != columns-1:
+            column += 1
+        else:
+            row += 1
+    return hypotenuses2
+
+
+def find_hypotenuse1(s):
+    turn = {HUMAN: COMPUTER, COMPUTER: HUMAN}
+    return any(map(lambda i: max_len(hypotenuse1_func(s)[i], turn[s.playTurn]) >= 4, range(rows + columns - 2 * 3 - 1)))
+
+def find_hypotenuse2(s):
+    turn = {HUMAN: COMPUTER, COMPUTER: HUMAN}
+    return any(map(lambda i: max_len(hypotenuse2_func(s)[i], turn[s.playTurn]) >= 4, range(rows + columns - 2 * 3 - 1)))
+
+
+def max_len(_list, num):
     count = 0
     max_count = 0
     for item in _list:
         if item != num:
             count = 0
         else:
-            count+=1
-        max_count = max(count,max_count)
+            count += 1
+        max_count = max(count, max_count)
     return max_count
 
 
-
-
 def printState(s):
-#Prints the board. The empty cells are printed as numbers = the cells name(for input)
-#If the game ended prints who won.
-        for r in range(rows):
-            print("\n|",end="")
-        #print("\n",len(s[0][0])*" --","\n|",sep="", end="")
-            for c in range(columns):
-                if s.board[r][c]==COMPUTER:
-                    print("X|", end="")
-                elif s.board[r][c]==HUMAN:
-                    print("O|", end="")
-                else:
-                    print(" |", end="")
+    # Prints the board. The empty cells are printed as numbers = the cells name(for input)
+    # If the game ended prints who won.
+    for r in range(rows):
+        print("\n|", end="")
+        # print("\n",len(s[0][0])*" --","\n|",sep="", end="")
+        for c in range(columns):
+            if s.board[r][c] == COMPUTER:
+                print("X|", end="")
+            elif s.board[r][c] == HUMAN:
+                print("O|", end="")
+            else:
+                print(" |", end="")
 
-        print()
+    print()
 
-        for i in range(columns):
-            print(" ",i,sep="",end="")
+    for i in range(columns):
+        print(" ", i, sep="", end="")
 
-        print()
-        
-        val=value(s)
+    print()
 
-        if val==VICTORY:
-            print("I won!")
-        elif val==LOSS:
-            print("You beat me!")
-        elif val==TIE:
-            print("It's a TIE")
+    val = value(s)
 
+    if val == VICTORY:
+        print("I won!")
+    elif val == LOSS:
+        print("You beat me!")
+    elif val == TIE:
+        print("It's a TIE")
 
 
 def isFinished(s):
-#Seturns True iff the game ended
-        return value(s) in [LOSS, VICTORY, TIE] or s.size==0
+    # Seturns True iff the game ended
+    return value(s) in [LOSS, VICTORY, TIE] or s.size == 0
 
 
 def isHumTurn(s):
-#Returns True iff it is the human's turn to play
-        return s.playTurn==HUMAN
-    
+    # Returns True iff it is the human's turn to play
+    return s.playTurn == HUMAN
 
 
 def decideWhoIsFirst(s):
-#The user decides who plays first
-        if int(input("Who plays first? 1-me / anything else-you : "))==1:
-            s.playTurn=COMPUTER
-        else:
-            s.playTurn=HUMAN
-            
-        return s.playTurn
-        
+    # The user decides who plays first
+    if int(input("Who plays first? 1-me / anything else-you : ")) == 1:
+        s.playTurn = COMPUTER
+    else:
+        s.playTurn = HUMAN
+
+    return s.playTurn
+
 
 def makeMove(s, c):
-#Puts mark (for huma. or comp.) in col. c
-#and switches turns.
-#Assumes the move is legal.
+    # Puts mark (for huma. or comp.) in col. c
+    # and switches turns.
+    # Assumes the move is legal.
 
-        r=0
-        while r<rows and s.board[r][c]==0:
-            r+=1
+    r = 0
+    while r < rows and s.board[r][c] == 0:
+        r += 1
 
-        s.board[r-1][c]=s.playTurn # marks the board
-        s.size -= 1 #one less empty cell
-        if (s.playTurn == COMPUTER ):
-            s.playTurn = HUMAN
-        else:
-            s.playTurn = COMPUTER
+    s.board[r - 1][c] = s.playTurn  # marks the board
+    s.size -= 1  # one less empty cell
+    if (s.playTurn == COMPUTER):
+        s.playTurn = HUMAN
+    else:
+        s.playTurn = COMPUTER
 
-   
+
 def inputMove(s):
-#Reads, enforces legality and executes the user's move.
+    # Reads, enforces legality and executes the user's move.
 
-        #self.printState()
-        flag=True
-        while flag:
-            c=int(input("Enter your next move: "))
-            if c<0 or c>=columns or s.board[0][c]!=0:
-                print("Illegal move.")
+    # self.printState()
+    flag = True
+    while flag:
+        c = int(input("Enter your next move: "))
+        if c < 0 or c >= columns or s.board[0][c] != 0:
+            print("Illegal move.")
 
-            else:
-                flag=False
-                makeMove(s,c)
+        else:
+            flag = False
+            makeMove(s, c)
 
-        
+
 def getNext(s):
-#returns a list of the next states of s
-        ns=[]
-        for c in list(range(columns)):
-            print("c=",c)
-            if s.board[0][c]==0:
-                print("possible move ", c)
-                tmp=cpy(s)
-                makeMove(tmp, c)
-                print("tmp board=",tmp.board)
-                ns+=[tmp]
-                print("ns=",ns)
-        print("returns ns ", ns)
-        return ns
+    # returns a list of the next states of s
+    ns = []
+    for c in list(range(columns)):
+        print("c=", c)
+        if s.board[0][c] == 0:
+            print("possible move ", c)
+            tmp = cpy(s)
+            makeMove(tmp, c)
+            print("tmp board=", tmp.board)
+            ns += [tmp]
+            print("ns=", ns)
+    print("returns ns ", ns)
+    return ns
 
-def inputComputer(s):    
-        return alphaBetaPruning.go(s)
+
+def inputComputer(s):
+    return alphaBetaPruning.go(s)
